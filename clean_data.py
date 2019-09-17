0742bc31d2c03cd87e75153956ee90938f89221d
Sebastian <seb.hollister@gatech.edu>
2019-09-17 18:45:10 -0400

    added more info for files, added example ratio calculation

--- clean_data.py
+++ clean_data.py
@@ -6,4 +6,13 @@ Returns: values needed for ratios which are
             *
             *
             *
- """
+"""
+
+
+"""Example of a function to calc P / E. 
+    For all these functions assume you have the necessary inputs
+    All of them will look similar to this one"""
+
+def calc_PE(market_value_per_share, EPS):
+    """TODO: Calc PE ratio and return the P / E ratio """
+    return 0
--- rank.py
+++ rank.py
@@ -1,1 +1,3 @@
-"""TODO: Take the ratios from calc_ratios.py, and do the 2 rounds of ranking for each stock"""
+"""TODO: Take the ratios from calc_ratios.py, and do the 2 rounds of ranking for each stock
+Assume you have all the necessary inputs from the previous file, like the ratio values for each company
+"""
