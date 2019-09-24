484224ae01baa77f856d8279f4eee3c26305dff3
Sebastian <seb.hollister@gatech.edu>
2019-09-24 18:46:44 -0400

    added in api call

--- get_datasets.py
+++ get_datasets.py
@@ -1,1 +1,8 @@
 """TODO: This file will connect with sources for data, and get the datasets"""
+import pandas as pd
+import requests
+import json 
+
+response = requests.get("https://financialmodelingprep.com/api/v3/company-key-metrics/AAPL?period=quarter")
+
+print(response.json())
