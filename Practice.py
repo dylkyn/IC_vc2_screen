87a91d246f0fa81049e0d5ee3e80f9a639479c81
Sam Garvis <samuelpaulgarvis@gmail.com>
2020-03-09 17:34:06 -0400

    Close to Perfect

--- Practice.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = {'name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'], 
-        'year': [2012, 2012, 2013, 2014, 2014], 
-        'reports': [4, 24, 31, 2, 3]}
-df = pd.DataFrame(data, index = ['Cochice', 'Pima', 'Santa Cruz', 'Maricopa', 'Yuma'])
-print(df.head())
-df = df.drop("Cochice")
-print(df.head())
